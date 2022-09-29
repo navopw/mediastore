31223611f7e43d34dcaaff6fe36862250fc6212f
Stanislav Hetzel <stanislav.hetzel@ffuf.de>
2022-09-29 21:26:22 +0200

    Migration

---
+++ migrations/1664370072_collections_snapshot.go
@@ -0,0 +1,241 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+// Auto generated migration with the most recent collections configuration.
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `[
+			{
+				"id": "systemprofiles0",
+				"created": "2022-09-28 12:51:40.320",
+				"updated": "2022-09-28 12:51:40.320",
+				"name": "profiles",
+				"system": true,
+				"schema": [
+					{
+						"system": true,
+						"id": "pbfielduser",
+						"name": "userId",
+						"type": "user",
+						"required": true,
+						"unique": true,
+						"options": {
+							"maxSelect": 1,
+							"cascadeDelete": true
+						}
+					},
+					{
+						"system": false,
+						"id": "pbfieldname",
+						"name": "name",
+						"type": "text",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "pbfieldavatar",
+						"name": "avatar",
+						"type": "file",
+						"required": false,
+						"unique": false,
+						"options": {
+							"maxSelect": 1,
+							"maxSize": 5242880,
+							"mimeTypes": [
+								"image/jpg",
+								"image/jpeg",
+								"image/png",
+								"image/svg+xml",
+								"image/gif"
+							],
+							"thumbs": null
+						}
+					}
+				],
+				"listRule": "userId = @request.user.id",
+				"viewRule": "userId = @request.user.id",
+				"createRule": "userId = @request.user.id",
+				"updateRule": "userId = @request.user.id",
+				"deleteRule": null
+			},
+			{
+				"id": "ns6l8fjb1mpr5m9",
+				"created": "2022-09-28 13:01:01.939",
+				"updated": "2022-09-28 13:01:01.939",
+				"name": "media",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "hdol1les",
+						"name": "user_id",
+						"type": "text",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "mqgnwfzo",
+						"name": "hash",
+						"type": "text",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "bousptdb",
+						"name": "name",
+						"type": "text",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "avm326ok",
+						"name": "mime",
+						"type": "text",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "2qmqzlfn",
+						"name": "file_size",
+						"type": "number",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null
+						}
+					},
+					{
+						"system": false,
+						"id": "2qjlf0yq",
+						"name": "preview_id",
+						"type": "text",
+						"required": true,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "tprcdncz",
+						"name": "camera_model",
+						"type": "text",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "1ymep6w9",
+						"name": "focal_length",
+						"type": "text",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "zuek08ja",
+						"name": "exif_date",
+						"type": "text",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null
+						}
+					},
+					{
+						"system": false,
+						"id": "mrtdpyl6",
+						"name": "latitude",
+						"type": "number",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null
+						}
+					},
+					{
+						"system": false,
+						"id": "odahyys1",
+						"name": "longitude",
+						"type": "number",
+						"required": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null
+						}
+					}
+				],
+				"listRule": null,
+				"viewRule": null,
+				"createRule": null,
+				"updateRule": null,
+				"deleteRule": null
+			}
+		]`
+
+		collections := []*models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collections); err != nil {
+			return err
+		}
+
+		return daos.New(db).ImportCollections(collections, true, nil)
+	}, func(db dbx.Builder) error {
+		// no revert since the configuration on the environment, on which
+		// the migration was executed, could have changed via the UI/API
+		return nil
+	})
+}
